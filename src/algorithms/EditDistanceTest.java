630e5748a1e716df81cc6f3b6c518c5720cfbc3d
Sowseelya <sowseelyav@gmail.com>
2026-09-18 09:19:31 +0530

    Update EditDistanceTest.java

--- src/algorithms/EditDistanceTest.java
+++ src/algorithms/EditDistanceTest.java
@@ -1,65 +1,19 @@
 package algorithms;
 
 public class EditDistanceTest {
+
     public static void main(String[] args) {
 
-        String[] queries = {
-            "Java",
-            "Jvaa",
-            "Python",
-            "Pythn",
-            "Machine Learning",
-            "Machne Learning",
-            "Data Science",
-            "Data Scince"
-        };
+        String str1 = "kitten";
+        String str2 = "sitting";
 
-        String[] keywords = {
-            "Java",
-            "Python",
-            "Machine Learning",
-            "Data Science"
-        };
+        int distance = EditDistance.calculate(str1, str2);
 
-        System.out.println("======================================");
-        System.out.println("       EDIT DISTANCE TEST");
-        System.out.println("======================================");
-
-        for (int i = 0; i < queries.length; i++) {
-
-            String query = queries[i];
-
-            System.out.println("\nQuery: " + query);
-
-            String closestWord = "";
-            int smallestDistance = Integer.MAX_VALUE;
-
-            for (int j = 0; j < keywords.length; j++) {
-
-                int distance =
-                    EditDistance.calculate(query, keywords[j]);
-
-                System.out.println(
-                    "  " + keywords[j] +
-                    " -> Distance: " + distance
-                );
-
-                if (distance < smallestDistance) {
-                    smallestDistance = distance;
-                    closestWord = keywords[j];
-                }
-            }
-
-            System.out.println(
-                "Closest Match: " + closestWord
-            );
-
-            System.out.println(
-                "Minimum Distance: " + smallestDistance
-            );
-        }
-
-        System.out.println("\n======================================");
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+        System.out.println("Edit Distance: " + distance);
+    }
+}
         System.out.println("          TEST COMPLETED");
         System.out.println("======================================");
     }
